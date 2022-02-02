393b1b56a45c46d9d3a1e6030814dfaf03c3c33a
yournewgoodname <ens-rostov@yandex.ru>
2022-02-02 22:29:46 +0300

    05-03 completed

--- 05-virt-04-docker-compose/src/terraform/output.tf
+++ 05-virt-04-docker-compose/src/terraform/output.tf
@@ -5,3 +5,11 @@ output "internal_ip_address_node01_yandex_cloud" {
 output "external_ip_address_node01_yandex_cloud" {
   value = "${yandex_compute_instance.node01.network_interface.0.nat_ip_address}"
 }
+
+output "internal_ip_address_node02_yandex_cloud" {
+  value = "${yandex_compute_instance.node02.network_interface.0.ip_address}"
+}
+
+output "external_ip_address_node02_yandex_cloud" {
+  value = "${yandex_compute_instance.node02.network_interface.0.nat_ip_address}"
+}
