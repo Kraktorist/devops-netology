703ad6047742e4635b6c14052b09aa9ded60834e
yournewgoodname <ens-rostov@yandex.ru>
2022-05-26 22:37:02 +0300

    remove turtle

--- plugins/modules/ansible_file.py
+++ plugins/modules/ansible_file.py
@@ -3,7 +3,7 @@
 # Copyright: (c) 2018, Terry Jones <terry.jones@example.org>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import (absolute_import, division, print_function)
-from turtle import width
+# from turtle import width
 __metaclass__ = type
 
 DOCUMENTATION = r'''
